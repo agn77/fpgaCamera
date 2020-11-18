e80c972b5a6536002c8ab4c4c61be47a726a6075
agn77 <a.gonzalez2345@gmail.com>
2020-11-17 22:57:51 -0800

    initial modules

---
+++ Library/frame_buffer.vhd
@@ -0,0 +1,100 @@
+-- create a buffer to store pixels data for a frame of 320x240 pixels;
+-- data for each pixel is 12 bits;
+-- that is 76800 pixels; hence, address is represented on 17 bits 
+-- (2^17 = 131072 > 76800);
+-- Notes: 
+-- 1) If we wanted to work with 640x480 pixels, that would require
+-- an amount of embedded RAM that is not available on the Cyclone IV E of DE2-115;
+-- 2) We create the buffer with 76800 by stacking-up two blocks
+-- of 2^16 = 65536 addresses; 
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+
+ENTITY frame_buffer IS
+  PORT
+  (
+    data     : IN STD_LOGIC_VECTOR (11 DOWNTO 0);
+    rdaddress : IN STD_LOGIC_VECTOR (16 DOWNTO 0);
+    rdclock   : IN STD_LOGIC;
+    wraddress : IN STD_LOGIC_VECTOR (16 DOWNTO 0);
+    wrclock   : IN STD_LOGIC;
+    wren     : IN STD_LOGIC;
+    q        : OUT STD_LOGIC_VECTOR (11 DOWNTO 0)
+  );
+END frame_buffer;
+
+
+ARCHITECTURE SYN OF frame_buffer IS
+
+  
+  COMPONENT my_frame_buffer_15to0 IS
+  PORT
+  (
+    data    : IN STD_LOGIC_VECTOR (11 DOWNTO 0);
+    rdaddress    : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+    rdclock    : IN STD_LOGIC ;
+    wraddress    : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+    wrclock    : IN STD_LOGIC  := '1';
+    wren    : IN STD_LOGIC  := '0';
+    q    : OUT STD_LOGIC_VECTOR (11 DOWNTO 0)
+  );
+  END COMPONENT;
+
+  
+  -- read signals
+  signal q_top : STD_LOGIC_VECTOR (11 DOWNTO 0);
+  signal q_bottom : STD_LOGIC_VECTOR (11 DOWNTO 0);
+  -- write signals
+  signal wren_top : STD_LOGIC;
+  signal wren_bottom : STD_LOGIC;
+  
+BEGIN
+
+  Inst_buffer_top : my_frame_buffer_15to0
+    PORT MAP (
+      data => data(11 downto 0),
+      rdaddress => rdaddress(15 downto 0),
+      rdclock => rdclock,
+      wraddress => wraddress(15 downto 0),
+      wrclock => wrclock,
+      wren => wren_top,
+      q => q_top
+    );
+  Inst_buffer_bottom : my_frame_buffer_15to0
+    PORT MAP (
+      data => data(11 downto 0),
+      rdaddress => rdaddress(15 downto 0),
+      rdclock => rdclock,
+      wraddress => wraddress(15 downto 0),
+      wrclock => wrclock,
+      wren => wren_bottom,
+      q => q_bottom
+    );  
+    
+  process (wraddress(16), wren)
+  begin
+    case wraddress(16) is 
+      when '0' =>
+        wren_top <= wren; wren_bottom <= '0';
+      when '1' =>
+        wren_top <= '0'; wren_bottom <= wren;  
+      when others =>
+        wren_top <= '0'; wren_bottom <= '0';
+    end case;
+  end process;
+  
+  process (rdaddress(16), q_top, q_bottom)
+  begin
+    case rdaddress(16) is 
+      when '0' =>
+        q <= q_top;
+      when '1' =>
+        q <= q_bottom;
+      when others =>
+        q <= "000000000000";
+    end case;
+  end process;
+    
+END SYN;
---
+++ Library/top_level.vhd
@@ -0,0 +1,211 @@
+
+-- reworked for DE10-Standard
+-- Alberto Gonzalez Navarro
+-- original Source: http://www.dejazzer.com/eigenpi/digital_camera/digital_camera.html
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity digital_cam_impl1 is
+  Port ( clk_50 : in  STD_LOGIC;
+    btn_resend          : in  STD_LOGIC;
+    led_config_finished : out STD_LOGIC;
+
+    vga_hsync : out  STD_LOGIC;
+    vga_vsync : out  STD_LOGIC;
+    vga_r     : out  STD_LOGIC_vector(7 downto 0);
+    vga_g     : out  STD_LOGIC_vector(7 downto 0);
+    vga_b     : out  STD_LOGIC_vector(7 downto 0);
+    vga_blank_N : out  STD_LOGIC;
+    vga_sync_N  : out  STD_LOGIC;
+    vga_CLK     : out  STD_LOGIC;
+
+    ov7670_pclk  : in  STD_LOGIC;
+    ov7670_xclk  : out STD_LOGIC;
+    ov7670_vsync : in  STD_LOGIC;
+    ov7670_href  : in  STD_LOGIC;
+    ov7670_data  : in  STD_LOGIC_vector(7 downto 0);
+    ov7670_sioc  : out STD_LOGIC;
+    ov7670_siod  : inout STD_LOGIC;
+    ov7670_pwdn  : out STD_LOGIC;
+    ov7670_reset : out STD_LOGIC
+  );
+end digital_cam_impl1;
+
+
+architecture my_structural of digital_cam_impl1 is
+
+
+  COMPONENT VGA
+  PORT(
+    CLK25 : IN std_logic;    
+    Hsync : OUT std_logic;
+    Vsync : OUT std_logic;
+    Nblank : OUT std_logic;      
+    clkout : OUT std_logic;
+    activeArea : OUT std_logic;
+    Nsync : OUT std_logic
+    );
+  END COMPONENT;
+
+  COMPONENT ov7670_controller
+  PORT(
+    clk : IN std_logic;
+    resend : IN std_logic;    
+    siod : INOUT std_logic;      
+    config_finished : OUT std_logic;
+    sioc : OUT std_logic;
+    reset : OUT std_logic;
+    pwdn : OUT std_logic;
+    xclk : OUT std_logic
+    );
+  END COMPONENT;
+
+  COMPONENT frame_buffer
+  PORT(
+    data : IN std_logic_vector(11 downto 0);
+    rdaddress : IN std_logic_vector(16 downto 0);
+    rdclock : IN std_logic;
+    wraddress : IN std_logic_vector(16 downto 0);
+    wrclock : IN std_logic;
+    wren : IN std_logic;          
+    q : OUT std_logic_vector(11 downto 0)
+    );
+  END COMPONENT;
+
+  COMPONENT ov7670_capture
+  PORT(
+    pclk : IN std_logic;
+    vsync : IN std_logic;
+    href : IN std_logic;
+    d : IN std_logic_vector(7 downto 0);          
+    addr : OUT std_logic_vector(16 downto 0);
+    dout : OUT std_logic_vector(11 downto 0);
+    we : OUT std_logic
+    );
+  END COMPONENT;
+
+  COMPONENT RGB
+  PORT(
+    Din : IN std_logic_vector(11 downto 0);
+    Nblank : IN std_logic;          
+    R : OUT std_logic_vector(7 downto 0);
+    G : OUT std_logic_vector(7 downto 0);
+    B : OUT std_logic_vector(7 downto 0)
+    );
+  END COMPONENT;
+
+  -- DE2-115 board has an Altera Cyclone V E, which has ALTPLL's'
+  COMPONENT my_altpll
+  PORT (
+    inclk0 : IN STD_LOGIC := '0';
+    c0     : OUT STD_LOGIC ;
+    c1     : OUT STD_LOGIC 
+    );
+  END COMPONENT;
+
+  COMPONENT Address_Generator
+  PORT(
+    CLK25       : IN  std_logic;
+    enable      : IN  std_logic;       
+    vsync       : in  STD_LOGIC;
+    address     : OUT std_logic_vector(16 downto 0)
+    );
+  END COMPONENT;
+
+
+
+  signal clk_50_camera : std_logic;
+  signal clk_25_vga    : std_logic;
+  signal wren       : std_logic;
+  signal resend     : std_logic;
+  signal nBlank     : std_logic;
+  signal vSync      : std_logic;
+
+  signal wraddress  : std_logic_vector(16 downto 0);
+  signal wrdata     : std_logic_vector(11 downto 0);   
+  signal rdaddress  : std_logic_vector(16 downto 0);
+  signal rddata     : std_logic_vector(11 downto 0);
+  signal red,green,blue : std_logic_vector(7 downto 0);
+  signal activeArea : std_logic;
+   
+
+begin
+
+
+  vga_r <= red(7 downto 0);
+  vga_g <= green(7 downto 0);
+  vga_b <= blue(7 downto 0);
+   
+
+  Inst_vga_pll: my_altpll PORT MAP(
+    inclk0 => clk_50,
+    c0 => clk_50_camera,
+    c1 => clk_25_vga
+  );    
+    
+    
+  -- take the inverted push button because KEY0 on DE2-115 board generates
+  -- a signal 111000111; with 1 with not pressed and 0 when pressed/pushed;
+  resend <= not btn_resend;
+  vga_vsync <= vsync;
+  vga_blank_N <= nBlank;
+  
+  Inst_VGA: VGA PORT MAP(
+    CLK25      => clk_25_vga,
+    clkout     => vga_CLK,
+    Hsync      => vga_hsync,
+    Vsync      => vsync,
+    Nblank     => nBlank,
+    Nsync      => vga_sync_N,
+    activeArea => activeArea
+  );
+
+  Inst_ov7670_controller: ov7670_controller PORT MAP(
+    clk             => clk_50_camera,
+    resend          => resend,
+    config_finished => led_config_finished,
+    sioc            => ov7670_sioc,
+    siod            => ov7670_siod,
+    reset           => ov7670_reset,
+    pwdn            => ov7670_pwdn,
+    xclk            => ov7670_xclk
+  );
+   
+  Inst_ov7670_capture: ov7670_capture PORT MAP(
+    pclk  => ov7670_pclk,
+    vsync => ov7670_vsync,
+    href  => ov7670_href,
+    d     => ov7670_data,
+    addr  => wraddress,
+    dout  => wrdata,
+    we    => wren
+  );
+
+  Inst_frame_buffer: frame_buffer PORT MAP(
+    rdaddress => rdaddress,
+    rdclock   => clk_25_vga,
+    q         => rddata,      
+    wrclock   => ov7670_pclk,
+    wraddress => wraddress(16 downto 0),
+    data      => wrdata,
+    wren      => wren
+  );
+  
+  Inst_RGB: RGB PORT MAP(
+    Din => rddata,
+    Nblank => activeArea,
+    R => red,
+    G => green,
+    B => blue
+  );
+
+  Inst_Address_Generator: Address_Generator PORT MAP(
+    CLK25 => clk_25_vga,
+    enable => activeArea,
+    vsync => vsync,
+    address => rdaddress
+  );
+
+end my_structural;
